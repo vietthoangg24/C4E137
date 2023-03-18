40c1fafb9a7775c08dcd4e3caac94db48928a7aa
vietthoang24 <vietthoangg2405@gmail.com>
2023-03-18 22:25:03 +0700

    test

---
+++ btvn/baitapvenha15/lesson6/app.js
@@ -0,0 +1,117 @@
+// console.log("welcome to javascript");
+// console.log("hello");
+// console.log("This is test text 😘");
+
+// console.error("day la 1 loi");
+// console.warn(" đây là cảnh báo");
+
+// /* tên biến bắt đầu bằng chữ cái "$"  "_"
+// trong biến có thể sử dụng số  */
+
+// // tên biến phân biệt chữ hoa chữ thường 
+
+// let name;// khai báo  biến 
+// name = "giá trị mới của name";//gán giá trị cho biến
+// name = "hello this is something";
+
+// console.log(name);//hiển thị giá trị của biến name lên màn hình console
+// let age = 10; /*khai báo và gán giá trị cho biến*/
+
+// const hello = "cần lao vi liên thủ";// hằng số ko thể thay đổi giá trị 
+
+// console.log(hello);
+
+
+// kiểu số (number)
+let age = 28;
+
+let weight = 71;
+
+let height = 170;
+
+let breast = 90;
+
+// kiểu chữ (string)
+let slogan = "cần lao vi liên thủ ";
+
+let name ="Hoàngg";
+
+let address = "Thái Bình";
+
+let phone = "+84 344534543554";
+
+// kiểu (boolean) 
+let hasgrilfriend = false;
+
+// kiểu null
+let girlfriend = null;
+
+let money = null;
+
+// kiểu indefined
+let future;
+let wife;
+
+// let a = 10;
+// let b = '10';
+// console.log(a == b); //toán tử so sánh cho ra 1 giá trị boolean
+
+// let x = true;
+// let y = true;
+// let z = false;
+
+// console.log (x && y && z);
+// console.log (x && y && !z);
+// console.log ((x && y) || z);
+// console.log ((x && y) || !z);
+// console.log (x && (y || z));
+
+// let name1 = 'AAA';
+// let age1 = 20;
+// let isGood = true;
+
+// console.log (age1 + 10 );
+// console.log(age1 % 3 );
+// console.log(name1 + ' BBB')
+// console.log(!isGood);
+
+// console.log(name1 == 'aaa' && age1 >=20 );
+// console.log(name1 != 'aaa' &&isGood  )
+// console.log(!(age < 10 ) && !isGood);
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = '40';
+
+// console.log(a + b + c ); //60
+// console.log(a-b/c); // 9.3
+// console.log(a - (b*c)); // -590
+// console.log(d-(a * b)-c); // -190
+// console.log(a + b + c + d); // 6040
+// console.log(d + a + b + c); // 40102030
+// console.log(d + a - b + c); // 4020
+// console.log(a - b + d - c); // -1070 
+// console.log(d + a - b + c); // 4020
+// console.log(d - c + a - b);// 0
+// console.log(a * b + d * c); // 1400
+
+
+
+let a = 1;
+let b = '2';
+let c =  3;
+let d = '4';
+
+console.log(b + d);//24
+console.log(a + b + c + d );//1234
+console.log(a - b + c - d);//-2
+console.log(a - b - c + d);// -44
+console.log( (b + d) - (a + c) ); // 20
+console.log( (a + b) - (c + d) ); // -22
+console.log(a * c + b * d);// 11
+console.log(-b + d); //-24
+console.log(-b - d);// -6
+console.log(-(b + d)); //-24
+
